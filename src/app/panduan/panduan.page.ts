6e7486953c150de604bc98b84238b384c59434d7
Mostoples <mostoplesam@gmail.com>
2019-07-22 04:41:49 +0700

    Tampilan panduan

--- src/app/panduan/panduan.page.ts
+++ src/app/panduan/panduan.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
+import { PDM } from './panduan.model';
+
 
 @Component({
   selector: 'app-panduan',
@@ -6,10 +10,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panduan.page.scss'],
 })
 export class PanduanPage implements OnInit {
+  pdmMaba: PDM[] = [
+    {
+      bab : '',
+      icon : '',
+      page : 1,
+      content : ['jaja','ahha','jajaj']
+    },
+    {
+      bab : '',
+      icon : '',
+      page : 1,
+      content : ['jaja','ahha','jajaj']
+    }
+  ];
+
+  pdmKambing: PDM[] = [
+    {
+      bab : '',
+      icon : '',
+      page : 1,
+      content : ['jaja','ahha','jajaj']
+    },
+    {
+      bab : '',
+      icon : '',
+      page : 1,
+      content : ['jaja','ahha','jajaj']
+    }
+  ];
+  
+
+  @ViewChild('slides') slider: IonSlides;
+  segment = 0;
+
+  choice: string = 'pilihan';
 
   constructor() { }
 
+  async segmentChanged() {
+    await this.slider.slideTo(this.segment);
+  }
+
+  async slideChanged() {
+    this.segment = await this.slider.getActiveIndex();
+  }
+
   ngOnInit() {
+  }
+
+  changeState(pilihan: number){
+    if(pilihan === 1){
+      this.choice = 'maba';
+    }else if(pilihan === 2){
+      this.choice = 'kambing';
+    }
+    
   }
 
 }
