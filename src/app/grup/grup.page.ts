8eeb037eb5c1e3760bc8d825e903d764a3b2aec7
Nearシ <49648184+dimasgalang@users.noreply.github.com>
2019-07-22 10:52:00 +0700

    Update Gruping

--- src/app/grup/grup.page.ts
+++ src/app/grup/grup.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { RemoteServiceService } from '../remote-service.service';
 
 @Component({
   selector: 'app-grup',
-  templateUrl: './grup.page.html',
-  styleUrls: ['./grup.page.scss'],
+  templateUrl: 'grup.page.html',
+  styleUrls: ['grup.page.scss'],
 })
-export class GrupPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class GrupPage {
+  data1: any;
+  data2: any;
+  data3: any;
+  data4: any;
+  constructor(public api: RemoteServiceService, public loadingController: LoadingController) {
   }
-
+  async getData() {
+    const loading = await this.loadingController.create({
+      message: 'Loading'
+    });
+    await loading.present();
+    this.api.getData()
+      .subscribe(res => {
+        console.log(res);
+        this.data1 = res[0];
+        this.data2 = res[1];
+        this.data3 = res[2];
+        this.data4 = res[3];
+        loading.dismiss();
+      }, err => {
+        console.log(err);
+        loading.dismiss();
+      });
+  }
+  ngOnInit() {
+    this.getData();
+  }
 }
