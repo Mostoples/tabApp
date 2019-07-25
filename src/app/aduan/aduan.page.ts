4d58c9d8841f773e831618f84de6eb6bf44328ae
Mostoples <mostoplesam@gmail.com>
2019-07-26 01:03:27 +0700

    view aduan

--- src/app/aduan/aduan.page.ts
+++ src/app/aduan/aduan.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Component({
   selector: 'app-aduan',
   templateUrl: './aduan.page.html',
   styleUrls: ['./aduan.page.scss'],
 })
+
 export class AduanPage implements OnInit {
 
-  constructor() { }
+  isiEmail: string = '';
+  subject: string = '';
+  
 
-  ngOnInit() {
+  constructor(private emailComposer: EmailComposer) {}
+
+  send(){
+     let email = {
+       to: 'mostoplesam@gmail.com',
+       subject: this.subject,
+       body: this.isiEmail,
+       isHtml: true
+     }
+     
+     this.emailComposer.open(email);
+
   }
+
+  ngOnInit(){
+
+  }
+
 
 }
