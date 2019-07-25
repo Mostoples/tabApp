9575b4884ad48cb453bd056f5340b6cf10a8eb4f
Mostoples <mostoplesam@gmail.com>
2019-07-26 00:56:23 +0700

    add page aduan

---
+++ src/app/aduan/aduan.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aduan',
+  templateUrl: './aduan.page.html',
+  styleUrls: ['./aduan.page.scss'],
+})
+export class AduanPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuardService] },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'aduan', loadChildren: './aduan/aduan.module#AduanPageModule' },
+
 
 
 ];
