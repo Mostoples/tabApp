be3c100eb5c405aeb192f8ee9a7672ad5f723f0a
Rifqy Abdallah <budiagus2410@student.uns.ac.id>
2019-08-06 18:59:13 +0700

    btoa

--- src/app/presensi/presensi.page.ts
+++ src/app/presensi/presensi.page.ts
@@ -9,16 +9,17 @@ const tipe = 'absensi2.php';
 @Component({
   selector: 'app-presensi',
   templateUrl: './presensi.page.html',
-  styleUrls: ['./presensi.page.scss'],
+  styleUrls: ['./presensi.page.scss']
 })
 export class PresensiPage {
   qrData = null;
   scanResult = 'Tidak ada hasil. Scanning aja kagak.';
   data: any = {};
+  apapa = '';
 
   scanCode() {
     this.barcodeScanner.scan().then(barcodeData => {
-      this.qrData = barcodeData.text;
+      this.qrData = atob(atob(barcodeData.text));
       this.storage.get('USER_INFO').then((res) => {
         const postData = JSON.stringify({QRcode: this.qrData, IDPeserta: res.ID_PESERTA, Group: res.ID_GROUP});
         this.http.post(goToHttp + tipe, postData).subscribe(data => {
@@ -34,6 +35,7 @@ export class PresensiPage {
       this.scanResult = err;
     });
   }
+
   constructor(
     private barcodeScanner: BarcodeScanner,
     private http: HttpClient,
